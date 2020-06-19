447aed02d1448d186fd75b86a49fe12d362fe5ca
Josselyn Stefania Troya Fuentes <stefani_troya@outlook.com>
2020-06-19 12:29:06 -0500

    agregar boton

--- BrendaArcentalesPryFraccion/src/pryfraccion/CapaInterfaz/WinFraccion.java
+++ BrendaArcentalesPryFraccion/src/pryfraccion/CapaInterfaz/WinFraccion.java
@@ -49,6 +49,7 @@ public class WinFraccion extends javax.swing.JFrame {
         btnIgual = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("APLICACION DE UNA CALCULADORA DE FRACCIONES");
@@ -182,6 +183,13 @@ public class WinFraccion extends javax.swing.JFrame {
             }
         });
 
+        jButton3.setText("Stefania");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -225,7 +233,8 @@ public class WinFraccion extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(76, 76, 76))))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnIgual, javax.swing.GroupLayout.PREFERRED_SIZE, 231, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -272,7 +281,9 @@ public class WinFraccion extends javax.swing.JFrame {
                         .addGap(5, 5, 5)
                         .addComponent(jButton1)
                         .addGap(18, 18, 18)
-                        .addComponent(jButton2)))
+                        .addComponent(jButton2)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton3)))
                 .addContainerGap(41, Short.MAX_VALUE))
         );
 
@@ -390,6 +401,10 @@ public class WinFraccion extends javax.swing.JFrame {
         txtPantalla.setText("Creado por Grupo4");
     }//GEN-LAST:event_jButton2ActionPerformed
 
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton3ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -441,6 +456,7 @@ public class WinFraccion extends javax.swing.JFrame {
     private javax.swing.JButton btnValor;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JTextField txtPantalla;
     // End of variables declaration//GEN-END:variables
 }
